74f31f1223136809452e2dba1cdb5d6b3ae4fb67
fedestol <101721014+fedestol@users.noreply.github.com>
2022-05-20 10:43:45 -0300

    commit all
    commit all

---
+++ Assets/Activator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //targetObject.SetActive(false);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // targetObject.SetActive(false);
+            targetObject.SetActive(!targetObject.activeInHierarchy);
+        }
+    }
+}
